7804b2dfef46f9e3c88db06e494cc79fdc62072c
Abdul Basit Dogar <113461803+basir2353@users.noreply.github.com>
2025-06-03 13:31:32 +0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
   try {
     const decoded = jwt.verify(token, 'your_jwt_secret_keyssssssssssssswwwwwwwwwweeeeee');
     req.user = decoded;
